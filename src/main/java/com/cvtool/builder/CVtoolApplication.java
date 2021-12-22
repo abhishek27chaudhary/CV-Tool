f111c4204793022c4445810da75b0f6a47726cc2
abhishekchaudhary <abhishekchaudhary@192.168.0.100>
2021-12-23 00:21:37 +0530

    commit -1

---
+++ src/main/java/com/cvtool/builder/CVtoolApplication.java
@@ -0,0 +1,13 @@
+package com.cvtool.builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CVtoolApplication.class, args);
+	}
+
+}
